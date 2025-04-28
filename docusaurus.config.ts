d2d1d4d1f8253204f9cdc3c5fdae8d42345921b2
JayClock <35857909+JayClock@users.noreply.github.com>
2025-04-28 13:25:52 +0800

    fix: update edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-              'https://github.com/unit-mesh/auto-dev/tree/master/docs/',
+              'https://github.com/unit-mesh/autodev-docs/tree/master/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
